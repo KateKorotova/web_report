d3655e7f09a0be6db72432c84c1f8fc13994105b
Yekatierina Korotova <yekaterina.korotova@gmail.com>
2024-07-04 10:19:16 +0200

    Add boostrap and inheritance to templates

--- app/routes.py
+++ app/routes.py
@@ -13,10 +13,11 @@ def index():
 @bp.route('/report/')
 def report():
     order = request.args.get('order', 'asc')
+    order = request.args.get('order', 'asc') if request.args.get('order', 'asc') else 'asc'
     sort_asc = order == 'asc'
     report_data = build_report(DATA_FOLDER)
     sorted_report = report_data.sort_values(by='lap_time', ascending=sort_asc)
-    return render_template('report.html', report=sorted_report)
+    return render_template('report.html', report=sorted_report, order=order)
 
 
 @bp.route('/report/drivers/')
@@ -24,7 +25,8 @@ def drivers():
     driver_id = request.args.get('driver_id')
     order = request.args.get('order', 'asc')
     sort_asc = order == 'asc'
-    report_data = build_report(DATA_FOLDER).sort_values(by='abbr_driver', ascending=sort_asc)
+    report_data = build_report(DATA_FOLDER).sort_values(
+        by='abbr_driver', ascending=sort_asc)
 
     if driver_id:
         driver_data = report_data[report_data['abbr_driver'] == driver_id]
@@ -32,5 +34,6 @@ def drivers():
             return "Driver ID not found", 404
         return render_template('driver_info.html', driver=driver_data)
     else:
-        drivers_list = report_data[['abbr_driver','driver']].sort_values(by='driver', ascending=sort_asc)
-        return render_template('drivers.html', drivers=drivers_list)
+        drivers_list = report_data[['abbr_driver', 'driver']].sort_values(
+            by='driver', ascending=sort_asc)
+        return render_template('drivers.html', drivers=drivers_list, order=order)
